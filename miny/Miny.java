3d6c031ec3f5d4f43448e1adbff597dbbf1141cf
Daniel Hoch <55744172+Derakveys@users.noreply.github.com>
2020-03-30 09:13:19 +0200

    Add files via upload

---
+++ miny/Miny.java
@@ -0,0 +1,15 @@
+
+package miny;
+
+
+public class Miny {
+    
+    public static void main(String[] args) {
+        Mines a = new Mines();
+        a.minegen();
+        a.evaluate();
+        a.count();
+        a.draw();
+    }
+    
+}
